9620c6af8670666248a5d7ab1da8291a45750d7a
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 20:54:29 -0400

    redeploy

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -1,25 +1,12 @@
 from http.server import BaseHTTPRequestHandler
-from urllib import parse
-import platform
 from datetime import datetime
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        query_string_list = parse.parse.qsl(url_components.query)
-        dic = dict(query_string_list)
-        name = dic.get("name")
-        message = ""
-        if name:
-            message = f"Aloha {name}"
-        else:
-            message = "Aloha stranger"
-
-        message += f"\nGreetings from Python version {platform.python_version()}"
-
+        #initial deployment
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
+        message = "Hello World!"
         self.wfile.write(message.encode())
         return
