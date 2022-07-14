fc986c405803e0a96cccf4e86932e6c9d2cd3b4c
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 22:23:15 -0400

    redeploy

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -6,28 +6,20 @@ import requests
 class handler(BaseHTTPRequestHandler):
     # http://localhost:3000/api/define?word=python
     def do_GET(self):
-        s = self.path
-        url_components = parse.urlsplit(s)
-        query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
-        print(dic)
-
-        if "country" in dic:
-            url = "https://restcountries.com/v3.1/name/"
-            # r = requests.get(url + dic["country"])
-            r = requests.get(url)
-            data = r.json()
+        url = "https://restcountries.com/v3.1/name/peru"
+        # r = requests.get(url + dic["country"])
+        r = requests.get(url)
+        data = r.json()
             # definitions = []
             # for word_data in data:
             #     definition = word_data["meanings"][0]["definitions"][0]["definition"]
             #     definitions.append(definition)
             # message = str(definitions)
-            message = data[0]
-            print("hi")
-            print(message)
 
+        if data:
+            message = data
         else:
-            message = "Give me a country to search please"
+            message = "Give me a country to search please :)"
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
