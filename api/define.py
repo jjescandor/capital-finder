c6046383a9d2a1826c7265b46a150479efce0037
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 23:11:03 -0400

    redeploy

--- api/define.py
+++ api/define.py
@@ -13,22 +13,18 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        if "word" in dic:
-            url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-            r = requests.get(url + dic["word"])
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
             data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = data
-            url1 = "https://restcountries.com/v3.1/name/peru"
-            r1 = requests.get(url1)
-            data1 = r1.json()
-            message = str(data1[0]["capital"])
-
+            capital = []
+            for c_data in data[0]:
+                definition = c_data["capital"]
+                capital.append(definition)
+            # message = str(data1[0]["capital"])
+            message = str(capital[0])
         else:
-            message = "Give me a word to define please 2"
+            message = "Give me a country to search please 2"
 
         self.send_response(200)
         self.send_header('Content-type','text/plain')
