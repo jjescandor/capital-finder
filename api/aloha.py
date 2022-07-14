b28dc2fb1061b712e4efc783bfbb6338cfaff63b
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 21:52:57 -0400

    redeploy

--- api/aloha.py
+++ api/aloha.py
@@ -5,6 +5,6 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = "Hello World! YAYYYYYYY"
+        message = "Hello World! Hola Mundo!"
         self.wfile.write(message.encode())
         return
--- api/capital-finder.py
+++ api/capital-finder.py
@@ -32,4 +32,3 @@ class handler(BaseHTTPRequestHandler):
         self.wfile.write(message.encode())
 
         return
-    
