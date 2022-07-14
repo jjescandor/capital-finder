fd18a8a7f2d59072cdc887f3276926613de0e0c0
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 21:45:53 -0400

    redeploy

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -11,15 +11,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        if "word" in dic:
-            url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-            r = requests.get(url + dic["word"])
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
             data = r.json()
-            definitions = []
-            for word_data in data:
-                definition = word_data["meanings"][0]["definitions"][0]["definition"]
-                definitions.append(definition)
-            message = str(definitions)
+            capital = data
+
+            # for word_data in data:
+            #     definition = word_data["meanings"][0]["definitions"][0]["definition"]
+            #     capital.append(definition)
+            message = str(capital[5])
 
         else:
             message = "Give me a word to define please"
