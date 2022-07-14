c69845d75fc6a154b4aed8c7c79e4f02f29f862e
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 21:54:33 -0400

    redeploy

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -21,7 +21,7 @@ class handler(BaseHTTPRequestHandler):
         # for word_data in data:
         #     definition = word_data["meanings"][0]["definitions"][0]["definition"]
         #     capital.append(definition)
-        message = f"The capital of {query_string_list} is {str(capital[5])}"
+        message = f"The capital of Peru is {str(capital[5])}"
         # else:
         # message = "Give me a country to search please"
 
