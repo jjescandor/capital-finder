84118b058083d0c817db61347925ad83831643a1
JJ Escandor <jj.p.escandor@gmail.com>
2022-07-13 20:56:04 -0400

    redeploy

--- api/capital-finder.py
+++ api/capital-finder.py
@@ -7,6 +7,25 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = "Hello World!"
+        message = "Hello World! how are you"
         self.wfile.write(message.encode())
         return
+
+        # s = self.path
+        # url_components = parse.urlsplit(s)
+        # query_string_list = parse.parse.qsl(url_components.query)
+        # dic = dict(query_string_list)
+        # name = dic.get("name")
+        # message = ""
+        # if name:
+        #     message = f"Aloha {name}"
+        # else:
+        #     message = "Aloha stranger"
+        #
+        # message += f"\nGreetings from Python version {platform.python_version()}"
+        #
+        # self.send_response(200)
+        # self.send_header('Content-type', 'text/plain')
+        # self.end_headers()
+        # self.wfile.write(message.encode())
+        # return
